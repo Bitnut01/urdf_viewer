631a0d19df773d7b72959f1c2d62c17bcba5e3fd
Bitnut01 <wmkuchar@gmail.com>
2025-08-08 16:45:00 +0200

    for tests

---
+++ launch/view.xarco.launch.py
@@ -0,0 +1,49 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution, FindExecutable
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg = FindPackageShare('urdf_viewer')
+    xacro_file = PathJoinSubstitution([pkg, 'urdf', 'test.urdf.xacro'])
+    rviz_config = PathJoinSubstitution([pkg, 'rviz', 'os1zed2.rviz'])
+
+    # (opcjonalnie) argumenty do xacro:
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    return LaunchDescription([
+        DeclareLaunchArgument('use_sim_time', default_value='false'),
+
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui'
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            parameters=[{
+                # uruchamiamy xacro i przekazujemy wynik jako string URDF
+                'robot_description': ParameterValue(
+                    Command([
+                        FindExecutable(name='xacro'),
+                        ' ',
+                        xacro_file
+                    ]),
+                    value_type=str
+                )
+            }]
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config],
+            output='screen'
+        ),
+    ])
